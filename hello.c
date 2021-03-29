1b430b0dccfbe896624b0a07b692b890f53adf96
nikolay.kuznetsov <nikolay.kuznetsov@osinit.com>
2021-03-29 18:56:20 +0300

    Fixing code

--- hello.c
+++ hello.c
@@ -23,40 +23,7 @@ int score = 0;
 int speed = 5;
 
 Snake *snake_body = NULL;
-/*struct snake
-{
-	int x;
-	int y;
 
-	struct snake *next;
-};
-typedef struct snake Snake;
-
-void add();
-Snake *snake = NULL;
-
-void add()
-{
-	if(snake == NULL) {
-		snake = (Snake *)malloc(sizeof(Snake));
-		snake->x = 4;
-		snake->y = 4;
-		snake->next = NULL;
-		return;
-	}
-
-	static int i = 0;
-	Snake *tmp = snake;
-	while(tmp->next != NULL){
-		tmp = tmp->next;
-	}
-	Snake *ttmp = (Snake *)malloc(sizeof(Snake));
-	ttmp->x = tmp->x;
-	ttmp->y = tmp->y;
-	ttmp->next = NULL;
-	tmp->next = ttmp;
-}
-*/
 void gen_fruit()
 {
 	int x;
@@ -71,7 +38,7 @@ void gen_fruit()
 	
 }
 
-void swap(int x, int y)
+void move_snake(int x, int y)
 {
 	bool isMoveX = true;
 	bool isMoveY = true;
@@ -130,6 +97,8 @@ void swap(int x, int y)
 		gen_fruit();
 }
 
+
+
 int main()
 {
 	int x = -1, y = 0;
@@ -142,17 +111,21 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 	start_color();
+
 	init_pair(1, COLOR_RED, COLOR_RED);
 	init_pair(2, COLOR_BLACK, COLOR_BLACK);
 	init_pair(3, COLOR_BLUE, COLOR_BLUE);
 	init_pair(4, COLOR_GREEN, COLOR_GREEN);
 	init_pair(5, COLOR_MAGENTA, COLOR_MAGENTA);
+
 	noecho();
 	halfdelay(speed);	
 	keypad(stdscr, TRUE);
+
 	add_tail(&snake_body);
 	add_tail(&snake_body);
 	add_tail(&snake_body);
+	
 	while(1){
 
 		erase();
@@ -187,16 +160,11 @@ int main()
 
 			printw("\n");
 
-			/*memcpy(h, field[i], 10);
-			attron(COLOR_PAIR(1));
-			printw("%s\n", h);
-			attroff(COLOR_PAIR(1));
-		*/
 
 		}
 		refresh();
 
-		if(score % 15 == 0 && is_change)
+		if(score % 5 == 0 && is_change)
 		{
 			halfdelay(--speed);
 			is_change = false;
@@ -228,7 +196,7 @@ int main()
 			x = 0;
 			y = -1;
 		}
-			swap(x, y);
+			move_snake(x, y);
 	}
 
 	getch();
